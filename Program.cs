0aa7188ad773a47ca5f8a6403d229613b9a2cf74
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-10-11 13:27:32 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿namespace arrayTaskClassroom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //string word = "Hdllo";
+            //char letter = 'e';
+            //bool isLetterHere = false;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] == letter)
+            //    {
+            //        isLetterHere = true;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(isLetterHere);
+            //task2
+            //string sentence = "Ya xochu spat";
+            //string findWord = "Ya";
+            //int j = 0;
+            //bool isWordHere = false;
+            //for (int i = 0; !isWordHere; i++)
+            //{
+            //    if (j == findWord.Length - 1)
+            //    {
+            //        isWordHere = true;
+            //        break;
+            //    }
+            //    if (sentence[i] == findWord[j])
+            //    {
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine(isWordHere);
+            //task 3
+            //string wordWithSpaces = " Slovo n ";
+            //string wordWithoutSpaces = "";
+            //for (int i = 0; i < wordWithSpaces.Length; i++)
+            //{
+            //    if (!(wordWithSpaces[i] == ' '))
+            //    {
+            //        wordWithoutSpaces += wordWithSpaces[i];
+            //    }
+            //}
+            //Console.WriteLine("String with spaces: " + wordWithSpaces);
+            //Console.WriteLine("String without spaces: " + wordWithoutSpaces);
+            ////task 4
+            //Console.WriteLine("\nEnter starting and ending number of range: ");
+            //int leftBoundary = Convert.ToInt32(Console.ReadLine());
+            //int rightBoundary = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("\nNumbers in the given range: ");
+            //for (int i = leftBoundary; i <= rightBoundary; i++)
+            //{
+            //    if (i < 0)
+            //    {
+            //        Console.WriteLine("Negative " + i + " Positive " + (i*-1)) ;
+            //    }
+            //    else if (i > 0)
+            //    {
+            //        Console.WriteLine("Negative " + (i * -1) + " Positive " + i);
+            //    }
+            //}
+            ////task 5
+            //Console.WriteLine("\nEnter word to reverse: ");
+            //string word = Console.ReadLine();
+            //string wordReversed = "";
+            //for (int i = word.Length - 1; i >= 0; i--)
+            //{
+            //    wordReversed += word[i];
+            //}
+            //Console.WriteLine("Reversed of your word: " + wordReversed);
+            //task 6
+            //Console.WriteLine("\nEnter size of your array: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[] nums = new int[size];
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine("Enter " + (i+1) + " element of array: ");
+            //    nums[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("\nYour array: ");
+            //for (int i = 0;i < nums.Length; i++)
+            //{
+            //    Console.WriteLine((i+1)+" element of array " + nums[i]);
+            //}
+            //int duplicateCount = 0;
+            //int temp = nums[0];
+            //int n = 0;
+            //while (duplicateCount<nums.Length)
+            //{
+            //    if (temp == nums[n])
+            //    {
+            //        duplicateCount++;
+            //    }
+            //    n++;
+            //}
+            //Console.WriteLine("\nThis array has "+duplicateCount+" duplicates");
+            //int[] array = { 10, 5, 10, 10, 2, 3, 4, 5, 5, 6, 7, 8, 9, 11, 12, 12 };
+            //int count = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int j = i; j < array.Length - 1; j++)
+            //    {
+            //        if (array[i] == array[j + 1])
+            //            count = count + 1;
+            //    }
+            //    Console.WriteLine("\t\n " + array[i] + "occurse" + count);
+            //    Console.ReadKey();
+            //}
+        }
+    }
+}
